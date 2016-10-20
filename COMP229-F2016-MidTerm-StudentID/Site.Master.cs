a62b4fbbad031c2fb2cfcb95fd47154b94e88629
Harkamalpreet <kaurharkamal96@gmail.com>
2016-10-19 20:59:50 -0400

    initial commit

--- COMP229-F2016-MidTerm-StudentID/Site.Master.cs
+++ COMP229-F2016-MidTerm-StudentID/Site.Master.cs
@@ -11,7 +11,28 @@ namespace COMP229_F2016_MidTerm_StudentID
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            SetActivePage();
+        }
 
+        /**
+        * This method adds a css class of "active" to list items
+        * relating to the current page
+        * 
+        * @private
+        * @method SetActivePage
+        * @return {void}
+        */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Todo List":
+                    todo.Attributes.Add("class", "active");
+                    break;
+            }
         }
     }
 }
